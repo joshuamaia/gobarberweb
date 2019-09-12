8352a6057b67ac813015053ca81c3265a3e3b5c8
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2019-09-12 09:07:33 -0300

    Validações

--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
@@ -8,10 +9,20 @@ export default function SignIn() {
     console.tron.log(data);
   }
 
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email('Insira um e-mail válido!')
+      .required('O e-mail é obrigatório!'),
+    password: Yup.string()
+      .min(6, 'Mínimo de 6 caracteres')
+      .required('A senha é obrigatória!'),
+    name: Yup.string().required('O nome é orbigatório!'),
+  });
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input
